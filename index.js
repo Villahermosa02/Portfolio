a9702055cd53e6fac1bf75cd043547449762f9d2
NIÑO FULTON RAGAY VILLAHERMOSA <45581901+Villahermosa02@users.noreply.github.com>
2025-07-14 21:40:47 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+    function redirectDrum() {
+      window.location.href = "https://villahermosa02.github.io/Virtual-drum/";
+    }
+
+     function redirectSimon() {
+      window.location.href = "https://villahermosa02.github.io/Simon-game/";
+    }
+
+     function redirectFigma(){
+      window.location.href = "https://www.figma.com/proto/hSx6DIqXloKqDYwFaN9hSJ/Untitled?node-id=1-2&p=f&t=ForPt0axuHZ7wZvV-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A2";
+    }
+
+    function redirectfb(){
+      window.location.href = "https://www.facebook.com/Ryota.Kise00";
+     }
+    function redirectmessenger(){
+      window.location.href = "https://m.me/Ryota.Kise00/";
+     }
+              function redirectgh(){
+      window.location.href = "https://github.com/Villahermosa02";
+     }
+
+
+    function downloadPDF() {
+    const link = document.createElement('a');
+    link.href = 'resume.pdf';
+    link.download = 'downloaded-file.pdf'; 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+     
